cedd700791163f6f83be99390f45f906f44fdb8a
Devansh <devanshbansal2021@gmail.com>
2025-11-27 11:59:12 +0530

    Set up cors for static azure website

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy  =>
                       {
-                          policy.WithOrigins("http://localhost:5173")
+                          policy.WithOrigins("http://localhost:5173", "https://red-coast-083a6fc0f.3.azurestaticapps.net")
                             .AllowAnyMethod()
                             .AllowAnyHeader();
                       });
