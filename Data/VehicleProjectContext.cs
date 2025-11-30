ac3596aac604aec59c974f223f63b4728623b37a
Devansh <devanshbansal2021@gmail.com>
2025-11-30 20:40:19 +0530

    Removed order, product and customer tables

--- Data/VehicleProjectContext.cs
+++ Data/VehicleProjectContext.cs
@@ -9,8 +9,5 @@ public class VehicleProjectContext : DbContext
         
     }
     public DbSet<Car> Cars {get; set;}
-    public DbSet<Order> Orders {get; set;}
-    public DbSet<Customer> Customers {get; set;}
-    public DbSet<Product> Products {get; set;}
 
 }
