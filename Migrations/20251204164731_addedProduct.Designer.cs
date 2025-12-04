22cb83e6f579e9c2541c749a1eebdc5f0b38a9fc
Devansh <devanshbansal2021@gmail.com>
2025-12-04 22:17:52 +0530

    Addedproduct table

--- Migrations/20251204164731_addedProduct.Designer.cs
+++ Migrations/20251204164731_addedProduct.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VehicleProject.Data;
 
@@ -10,9 +11,11 @@ using VehicleProject.Data;
 namespace VehicleProject.Migrations
 {
     [DbContext(typeof(VehicleProjectContext))]
-    partial class VehicleProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20251204164731_addedProduct")]
+    partial class addedProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
